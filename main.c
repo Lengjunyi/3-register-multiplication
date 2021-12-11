7b2e775c8389a52743825a3ca850d6cb81b17ece
Alan (Jiacheng) Liang <alan.liang20@imperial.ac.uk>
2021-12-11 01:38:55 +0000

    Implemented pushing of power of 2

--- main.c
+++ main.c
@@ -39,7 +39,8 @@
 int main() {
   int R0 = 0, R1 = 6, R2 = 7;
 
-  /* Step 1: Remove factors of 2 from R1 */
+  /* Step 1: Remove factors of 2 from R1 
+    Example: (6, 7) -> (3, 14) */
 
   // Divide R1 by 2
   L1: SUB(R1, L2, L3)
@@ -59,6 +60,20 @@ int main() {
   L12: ADD(R0, L13)
   L13: ADD(R0, L11)
   L14: ADD(R0, L15)
+  /* Now R0 * R2 = m0 * n0, and R0 is odd.
+  This is useful, and we use them as our new m and n. */
 
-  L15: HALT
+
+  /* Step 2: "Push" R2 onto R0, Transform it
+  into the form m * 2 ^ n
+    Example: (3, 14) -> (3 * 2 ^ 14, 0) */
+
+  L15: SUB(R2, L16, L21)
+  L16: SUB(R0, L17, L18)
+  L17: ADD(R1, L16)
+  L18: SUB(R1, L19, L15)
+  L19: ADD(R0, L20)
+  L20: ADD(R0, L18)
+
+  L21: HALT
 }
